80b6c9489f6c28e994e6a85c6b5b3a9d9a229877
CarlosElio <carloseliogonzalezapa1997@gmail.com>
2024-02-17 19:38:11 -0500

    Reto Semana 13

---
+++ app.js
@@ -0,0 +1,97 @@
+let codigo="";
+let url="https://api.unsplash.com/search/photos?page=1&query=category&client_id=PdJcmXctwT5CNahoKlmiP7lBK4nx6HwkYdF9VtLf6pM";
+
+
+//let Arregloimagenes=[];
+let buscarInput=document.querySelector("#buscar-input");
+let contenedorImagenesGlobal=document.querySelector("#contenedor-imagenes"); 
+
+
+
+buscarInput.addEventListener('keypress',(e)=>{
+    if(e.key==='Enter'){
+        readPhotos(e.target.value)
+    }
+
+
+});
+
+
+
+async function  readPhotos(busqueda){
+    const  response=  await fetch(`https://api.unsplash.com/search/photos?page=1&query=${busqueda}&client_id=PdJcmXctwT5CNahoKlmiP7lBK4nx6HwkYdF9VtLf6pM`);
+
+
+    const data=  await response.json();
+    let resultado=data.results;
+   // console.log(resultado);
+   //resultado.forEach(imagen => {
+   //        imagen=imagen.links.download;
+   //          Arregloimagenes.push(imagen);       
+   //     });
+   //     console.log(Arregloimagenes);
+     printHTML(resultado);
+ 
+
+}
+let contenedorImagenes;
+
+function  printHTML(resultado){
+    limpiarHTML();
+    console.log(resultado);
+    contenedorImagenes="";
+    
+    resultado.forEach(imagenes=>{
+        contenedorImagenes+=`<div class="imagen">
+                <img
+                  class="bg-image"
+                 src="${imagenes.links.download}"
+                alt=""
+                />
+                <div class="capa">
+               <button>Guardar</button>    
+             </div>
+            <div class="contenedor-iconos-imagenes">
+             <div class="icono-1">
+             <img src="img/descargas.svg">
+             </div>
+            <div class="icono-2">
+                 <img src="img/cinematografia.svg">
+                 </div>       
+                 </div>
+                </div>
+            `
+        });
+
+        contenedorImagenesGlobal.innerHTML=contenedorImagenes;
+    }
+
+        // contenedorImagenes.innerHTML=` 
+                        // <div class="imagen">
+                                // <img
+                                    //   class="bg-image"
+                                    //  src="${imagenes.links.download}"
+                                    // alt=""
+                                    // />
+                                // <div class="capa">
+                            //    <button>Guardar</button>    
+                                //  </div>
+                                // <div class="contenedor-iconos-imagenes">
+                                //  <div class="icono-1">
+                                //  <img src="img/descargas.svg">
+                                //  </div>
+                                // <div class="icono-2">
+                                // <img src="img/cinematografia.svg">
+                                // </div>       
+                            //  </div>
+                    // </div> 
+                    // `
+   
+
+
+
+
+
+function limpiarHTML(){
+    contenedorImagenesGlobal.innerHTML="";
+}
